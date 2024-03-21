361378a10983b802c90e475fb64cad6b4bc19305
Sunlution <sunface@outlook.kr>
2024-03-21 14:26:05 +0900

    [ADD] color added

---
+++ lib/app/resources/ui_config/color_config.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  AppColor._();
+  static const heartBlue = Color(0xFF70ABE6);
+  static const iconBlue = Color(0xFF4999E9);
+  static const blue = Color(0xFF5C8DD9);
+  static const red = Color(0xFFE5387F);
+}
